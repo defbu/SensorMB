99fa00a73f4d562a546e752b2598b5ea46557d88
Frank Buijze <msn@frajaweb.nl>
2018-09-04 23:55:45 +0200

    test

--- ultrasonic.ts
+++ ultrasonic.ts
@@ -17,7 +17,7 @@ namespace defbu {
      * @param trig pin connected to trig, eg: DigitalPin.P0
      * @param echo Pin connected to echo, eg: DigitalPin.P1
      */
-    //% subcategory="HC-SR04"
+    //% subcategory="HC-SR04 distance"
     //% blockId="defbu_get_distance" block="distance in %unit | trig %trig | echo %echo"
     //% trig.fieldEditor="gridpicker" trig.fieldOptions.columns=3
     //% trig.fieldOptions.tooltips="false"

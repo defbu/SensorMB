69d71c0c8bdad28ea56a68275182147c6c3e18eb
Frank Buijze <msn@frajaweb.nl>
2018-09-04 20:55:52 +0200

    Push

--- ultrasonic.ts
+++ ultrasonic.ts
@@ -31,7 +31,7 @@ namespace defbu {
         const MAX_DIST_MICROMETER = 3000 * 1000
         const VELOCITY_OF_SOUND = 343 // 343 m/s at sea level and 20 C
         const MAX_PULSE_DURATION_US = Math.idiv(2 * MAX_DIST_MICROMETER, VELOCITY_OF_SOUND)
-        /*
+
         //Pulse
         pins.setPull(trigPinNumber, PinPullMode.PullNone)
         pins.digitalWrite(trigPinNumber, 0)
@@ -40,7 +40,7 @@ namespace defbu {
         control.waitMicros(10)
         pins.digitalWrite(trigPinNumber, 0)
         control.waitMicros(0)
-
+        /*
         //Receive echo
         const pulseDuration = pins.pulseIn(echoPinNumber, PulseValue.Hight, MAX_PULSE_DURATION_US)
         let objectDistance = Math.idiv(pulseDuration * VELOCITY_OF_SOUND, 2)

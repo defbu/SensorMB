8e690cdcc4b47a46951cec329c0570391fa5815b
Frank Buijze <msn@frajaweb.nl>
2018-09-04 22:35:43 +0200

    Update

--- test.ts
+++ test.ts
@@ -2,4 +2,4 @@
  * Ultrasonic tests
  */
 
-const distance: number = defbu.getUltrasonicDistance(defbu.DistanceUnit.CM, defbu.Pin.P0, defbu.Pin.P1)
+const distance: number = defbu.getDistance(defbu.DistanceUnit.CM, defbu.Pin.P0, defbu.Pin.P1)
--- ultrasonic.ts
+++ ultrasonic.ts
@@ -24,7 +24,7 @@ namespace defbu {
     //% echo.fieldEditor="gridpicker" echo.fieldOptions.columns=3
     //% echo.fieldOptions.tooltips="false"
     //% weight=45
-    export function getUltrasonicDistance(unit: DistanceUnit, trig: Pin, echo: Pin): number {        
+    export function getDistance(unit: DistanceUnit, trig: Pin, echo: Pin): number {        
         const trigPinNumber: number = trig        
         const echoPinNumber: number = echo
         

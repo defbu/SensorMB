79aad24c181f02cf39d06db396259370d32f2756
Frank Buijze <msn@frajaweb.nl>
2018-09-10 20:11:03 +0200

    Temperature

--- ds18b20.ts
+++ ds18b20.ts
@@ -4,17 +4,17 @@ namespace defbu {
 
     export const enum TemperatureUnit {
         //% block="°C"
-        C = 1600,
+        C = "C",
         //% block="°dC"
-        DC = 160,
+        DC = "DC",
         //% block="°cC"
-        CC = 16,
+        CC = "CC",
         //% block="°F"
-        F = 100,
+        F = "F",
         //% block="°dF"
-        DF = 10,
+        DF = "DF",
         //% block="°cF"
-        CF = 1
+        CF = "CF"
     }
 
     //% shim=defbu::Temperature
@@ -25,37 +25,51 @@ namespace defbu {
 
     /**
      * Measures the temperature with DS18B20: range -55°C - 125 °C or -67°F - 257°F
-     * @param unit unit of distance, eg: defbu.TemperatureUnit.C
+     * @param unit unit of temperature, eg: defbu.TemperatureUnit.C
      * @param dataPin Data pin, eg: DigitalPin.P0
      */
-    //% blockId="defbu_get_temperature" block="temperature in %unit | data %dataPin"
+    //% blockId="defbu_get_temperature" block="temperature in %unit"
+    //% unit.fieldEditor="gridpicker" trig.fieldOptions.columns=3
+    //% unit.fieldOptions.tooltips="false"
+    //% group="Temperature"
+    //% weight=45
+    export function getTemperature(unit: defbu.TemperatureUnit = defbu.TemperatureUnit.C): number {        
+      return getTemperaturePin(unit,DigitalPin.P0)
+    }
+
+    
+    /**
+     * Measures the temperature with DS18B20: range -55°C - 125 °C or -67°F - 257°F
+     * @param unit unit of temperature, eg: defbu.TemperatureUnit.C
+     * @param dataPin Data pin, eg: DigitalPin.P0
+     */
+    //% blockId="defbu_get_temperaturepin" block="temperature in %unit | data %dataPin"
     //% unit.fieldEditor="gridpicker" trig.fieldOptions.columns=3
     //% unit.fieldOptions.tooltips="false"
     //% dataPin.fieldEditor="gridpicker" trig.fieldOptions.columns=3
     //% dataPin.fieldOptions.tooltips="false"
     //% group="Temperature"
     //% weight=45
-    export function getTemperature(unit: defbu.TemperatureUnit = defbu.TemperatureUnit.C, dataPin: DigitalPin = DigitalPin.P0): string {        
-        let temp = Temperature(dataPin);
-        let x = (temp / 100)
-        let y = (temp % 100)
-        let z = ''
-        if(temp >= 0){
-          if(y < 10){
-            z = x.toString() + '.0' + y.toString()
-          }
-          else{
-            z = x.toString() + '.' + y.toString()
-          }
-        }
-        else if(temp < 0){
-          if(y > -10){
-            z = '-' + (-x).toString() + '.0' + (-y).toString()
-          }
-          else{
-            z = '-' + (-x).toString() + '.' + (-y).toString()
-          }
-        }
-        return z
+    //% advanced=true
+    export function getTemperaturePin(unit: defbu.TemperatureUnit = defbu.TemperatureUnit.C, dataPin: DigitalPin = DigitalPin.P0): number {        
+      let temp = Temperature(dataPin);
+      if (unit == TemperatureUnit.C) {
+          return Math.idiv(temp , 100)
+      }
+      else if (unit == TemperatureUnit.DC) {
+          return Math.idiv(temp , 10)
+      }
+      else if (unit == TemperatureUnit.CC) {
+          return temp
+      }
+      else if (unit == TemperatureUnit.F) {
+          return Math.idiv(temp * 9, 500) + 32
+      }
+      else if (unit == TemperatureUnit.DF) {
+          return Math.idiv(temp * 9, 50) + 320
+      }
+      else {
+          return Math.idiv(temp * 9, 5) + 3200
+      } 
     }
 }
